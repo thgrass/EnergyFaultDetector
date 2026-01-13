06fea46a21162bc2cc21e14cb993227c44840fb7
croelofs <25582572+roelofsc@users.noreply.github.com>
2026-01-13 11:17:01 +0100

    Remove time features from configs to remove work arounds and improve docstrings and comments in PreDist.ipynb and predist_utils.py.

--- notebooks/PreDist/predist_utils.py
+++ notebooks/PreDist/predist_utils.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple
 from pathlib import Path
 from copy import deepcopy
-import logging
 import gc
 
 import pandas as pd
@@ -16,27 +15,36 @@ from energy_fault_detector.evaluation import PreDistDataset
 from energy_fault_detector.root_cause_analysis.arcana_utils import calculate_mean_arcana_importances
 
 
-def train_or_get_model(event_id: int, dataset: PreDistDataset, manufacturer: int, config_name: str, conf: Config,
-                       bottleneck_ratio: float, load_from_file: bool, ts_features_orig: List[str] | None
+def train_or_get_model(event_id: int, dataset: PreDistDataset, manufacturer: int, model_name: str, conf: Config,
+                       bottleneck_ratio: float, load_from_file: bool, time_features: List[str] | None
                        ) -> Tuple[int, FaultDetectionResult]:
-    """Processes a single event: loads data, trains/loads model, and predicts."""
+    """Processes a single event: loads data, trains/loads model, and predicts.
 
-    # Configure logging
-    logger = logging.getLogger('energy_fault_detector')
-    if not logger.handlers:
-        logging.basicConfig(level=logging.INFO)
+    Args:
+        event_id (int): ID of the event to process.
+        dataset (PreDistDataset): Dataset containing the event data.
+        manufacturer (int): Manufacturer ID for the event.
+        model_name (str): Name of the model to use for training.
+        conf (Config): Base configuration for training.
+        bottleneck_ratio (float): Ratio to determine the bottleneck size for the autoencoder.
+        load_from_file (bool): Whether to load the model from file if available. Otherwise, train and save a new model.
+        time_features (List[str] | None): List of time features to use for conditional autoencoders.
 
-    logger.info(f'Processing event {event_id} for manufacturer {manufacturer}...')
+    Returns:
+        Tuple[int, FaultDetectionResult]: A tuple containing the event ID and the fault detection result.
+    """
 
-    # Local copy of ts_features and configuration to avoid mutation issues in parallel
-    ts_features = ts_features_orig.copy() if ts_features_orig else None
+    # Local copy of time features and configuration to avoid mutation issues in parallel
+    ts_features = time_features.copy() if time_features else None
     local_conf = deepcopy(conf)
 
     # Get specific event data
     data = dataset.get_event_data(manufacturer, event_id)
+    train_data = data['train_data']
+    test_data = data['test_data']
 
     # Create a new model or load from file
-    model_path = Path(f'./models/m{manufacturer}/event_{event_id}/{config_name}')
+    model_path = Path(f'./models/m{manufacturer}/event_{event_id}/{model_name}')
 
     if model_path.exists() and load_from_file:
         model = FaultDetector()
@@ -45,23 +53,25 @@ def train_or_get_model(event_id: int, dataset: PreDistDataset, manufacturer: int
             with open(model_path / 'ts_features.txt', 'r') as f:
                 ts_features = f.read().splitlines()
     else:
-        # Prepare data and config
-        train_data = data['train_data']
+        # Add the code size to the AE configuration, based on the bottleneck ratio
+        # code_size is part of the model configuration, so we overwrite the parameter of the underlying dictionary.
         bottleneck = calculate_bottleneck(train_data, local_conf, bottleneck_ratio)
         local_conf['train']['autoencoder']['params']['code_size'] = bottleneck
+
+        # For the conditional autoencoders, add time features
         if ts_features:
             train_data = add_cyclic_time_features(train_data, ts_features)
 
         model = FaultDetector(local_conf, model_directory=model_path)
         model_data = model.fit(train_data, data['train_normal_flag'], save_models=True, overwrite_models=True)
         if ts_features:
-            # save the time features as well
+            # For the conditional autoencoders, save the time features as well
             with open(Path(model_data.model_path) / 'ts_features.txt', 'w') as f:
                 f.write('\n'.join(ts_features))
 
     # Predict
-    test_data = data['test_data']
     if ts_features:
+        # For the conditional autoencoders, add time features
         test_data = add_cyclic_time_features(test_data, ts_features)
     predictions = model.predict(test_data)
 
@@ -91,17 +101,41 @@ def add_cyclic_time_features(df: pd.DataFrame, features: List[str]) -> pd.DataFr
     return df
 
 
-def calculate_bottleneck(df: pd.DataFrame, config: Config, ratio: float = 0.75) -> int:
-    """Calculates code_size relative to input dimensions, accounting for exclusions/conditions."""
+def calculate_bottleneck(df: pd.DataFrame, config: Config, ratio: float) -> int:
+    """Calculates code_size (the bottleneck of the autoencoder) relative to input dimensions, accounting for excluded
+    features and conditional features.
+
+    Args:
+        df (pd.DataFrame): Input dataframe to determine the number of input features of the AE.
+        config (Config): Configuration for the AE.
+        ratio (float, optional): Ratio between input and bottleneck dimensions.
+
+    Returns:
+        int: The calculated bottleneck size for the autoencoder.
+    """
+
+    # Get the conditional features from the config
     ae_params = config['train']['autoencoder']['params']
     cond_features = ae_params.get('conditional_features', [])
 
-    # Exclude conditions and existing data_preprocessor exclusions
+    # Exclude conditions (not compressed)
     input_dim = len(df.columns) - len([c for c in cond_features if c in df.columns])
 
-    # Check for manual feature exclusions in config
-    dp_params = config['train'].get('data_preprocessor', {}).get('params', {})
-    excluded = dp_params.get('features_to_exclude', [])
+    # Check for feature exclusions in config
+    excluded = []
+    dp_config = config['train'].get('data_preprocessor', {})
+    if dp_config.get('params'):
+        # params-based data prep config
+        excluded = dp_config.get('params').get('features_to_exclude', [])
+    else:
+        # steps-based data prep config
+        steps = config['train']['data_preprocessor'].get('steps', [])
+        for step in steps:
+            if step['name'] == 'column_selector':
+                excluded = step['params'].get('features_to_exclude', [])
+                break
+
+    # Remove the excluded features from the input dimension
     input_dim -= len([e for e in excluded if e in df.columns])
 
     return max(1, round(input_dim * ratio))
@@ -110,7 +144,7 @@ def calculate_bottleneck(df: pd.DataFrame, config: Config, ratio: float = 0.75) 
 def find_optimal_threshold(true_anomalies: pd.Series,
                            max_criticalities: pd.Series,
                            thresholds: np.ndarray = np.arange(1, 100),
-                           k: int = 5) -> int:
+                           k: int = 5) -> Tuple[int, float]:
     """Finds the threshold maximizing reliability (Event-wise F0.5) using CV.
 
     Args:
@@ -120,35 +154,55 @@ def find_optimal_threshold(true_anomalies: pd.Series,
         k (int, optional): Number of folds for CV. Defaults to 5.
 
     Returns:
-        Optimal criticality threshold.
+        Optimal criticality threshold and avg validation reliability score.
     """
     y_true = true_anomalies.values
+    max_criticalities = max_criticalities.values
     skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
 
-    results = []
-    for t in thresholds:
-        fold_f05 = []
-        y_pred = (max_criticalities >= t).astype(int).values
+    chosen_thresholds = []
+    val_scores = []
 
-        for train_idx, val_idx in skf.split(y_true, y_true):
-            score = fbeta_score(y_true[val_idx], y_pred[val_idx], beta=0.5, zero_division=0)
-            fold_f05.append(score)
+    for train_idx, val_idx in skf.split(y_true, y_true):
+        y_train, max_crit_train = y_true[train_idx], max_criticalities[train_idx]
+        y_val, max_crit_val = y_true[val_idx], max_criticalities[val_idx]
 
-        results.append({'threshold': t, 'mean_f05': np.mean(fold_f05)})
+        # Best threshold on training data
+        best_t = None
+        best_train_f05 = -1.0
+        for t in thresholds:
+            y_pred_train = (max_crit_train >= t).astype(int)
+            f05_train = fbeta_score(y_train, y_pred_train, beta=0.5, zero_division=0)
+            if f05_train > best_train_f05:
+                best_train_f05 = f05_train
+                best_t = t
 
-    best_t = max(results, key=lambda x: x['mean_f05'])['threshold']
-    return best_t
+        chosen_thresholds.append(best_t)
+
+        # Evaluate on validation data
+        y_pred_val = (max_crit_val >= best_t).astype(int)
+        f05_val = fbeta_score(y_val, y_pred_val, beta=0.5, zero_division=0)
+        val_scores.append(f05_val)
+
+    robust_t = int(np.median(chosen_thresholds))
+    mean_val_f05 = float(np.mean(val_scores))
+
+    return robust_t, mean_val_f05
 
 
 def get_arcana_importances(manufacturer: int, event_id: int, config_name: str, data: pd.DataFrame) -> pd.Series:
+    """Get ARCANA importances for a given event."""
 
     model_path = Path(f'models/m{manufacturer}/event_{event_id}/{config_name}')
     model = FaultDetector()
     model.load_models(model_path)
+
+    # Load the time features and add them to the data if available (for the conditional autoencoders)
     if (model_path / 'ts_features.txt').exists():
         with open(model_path / 'ts_features.txt', 'r') as f:
             ts_features = f.read().splitlines()
         data = add_cyclic_time_features(data, ts_features)
+
     bias, _, _ = model.run_root_cause_analysis(data, track_losses=False, track_bias=False)
     return calculate_mean_arcana_importances(bias).sort_values(ascending=False)
 
@@ -156,12 +210,18 @@ def get_arcana_importances(manufacturer: int, event_id: int, config_name: str, d
 def calculate_earliness(criticality_threshold: int, report_ts: int | pd.Timestamp, criticality: pd.Series,
                         min_detection_time: pd.Timedelta = pd.Timedelta(hours=24)
                         ) -> Tuple[int | pd.Timestamp | None, float]:
-    """Calculate the detection time and earliness score.
+    """Calculate the detection time and earliness score:
+
+    E = max(0, min(1, (report_ts - detection_timestamp) / min_detection_time))
+
+    The earliness score is 1 if the fault is detected at least min_detection_time before the report and 0 if the
+    fault is detected after the report or not detected at all. Between min_detection_time before the report and the
+    report timestamp, the earliness score linearly decreases to 0.
 
     Args:
         criticality_threshold (int): Threshold for determining whether the event is detected.
         report_ts (int | pd.Timestamp): Timestamp of the report.
-        criticality (pd.Series): Series containing the criticality of each event.
+        criticality (pd.Series): Series containing the criticality pd.Series of each event.
         min_detection_time (pd.Timedelta, optional): Minimum detection time. Defaults to pd.Timedelta(hours=24).
 
     Returns:
@@ -177,6 +237,6 @@ def calculate_earliness(criticality_threshold: int, report_ts: int | pd.Timestam
 
     detection_timestamp = crit_threshold_reached.sort_index(ascending=True).index[0]
     detection_time = report_ts - detection_timestamp
-    # max(earliness, 0) to handle detection after fault is known
+    # max(earliness, 0) to handle detection after the fault is known
     earliness = max(min(1, detection_time / min_detection_time), 0)
     return detection_time, earliness
